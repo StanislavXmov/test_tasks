f2a72a8917e4f5ab8cec999c7fd240357b1696d3
stanislav <xiiiovart@gmail.com>
2024-06-09 14:17:03 +0300

    dp

---
+++ src/dp/fib.py
@@ -0,0 +1,18 @@
+class Solution:
+  def fib(self, n: int) -> int:
+    f0 = 0
+    f1 = 1
+
+    if n == 0:
+      return 0
+    
+    if n == 1:
+      return 1
+
+    while n > 1:
+      f1, f0 = f0 + f1, f1
+      n -= 1
+    
+    return f1
+  
+print(Solution.fib(Solution, 5))
