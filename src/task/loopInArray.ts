43d952d8a4d12d3079fc45edb315f128c0eee6e3
stanislav <xiiiovart@gmail.com>
2024-12-07 18:45:38 +0300

    loop

---
+++ src/task/loopInArray.ts
@@ -0,0 +1,27 @@
+const loopInArray = <T>(list: T[]) => {
+  const equality = <T>(list1: T[], list2: T[]) => {
+    for (let i = 0; i < list1.length; i++) {
+      if (list1[i] !== list2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    for (let j = i + 1; j < list.length; j++) {
+      const n1 = list[i];
+      const n2 = list[j];
+      if (n1 === n2) {
+        const list1 = list.slice(i, j);
+        const list2 = list.slice(j, j + (j - i));
+        if (list1.length === list2.length && list1.length > 2) {
+          if (equality(list1, list2)) {
+            return list1;
+          }
+        }
+      }
+    }
+  }
+  return [];
+}
